c346e29521165f14f2715079ed2cc6c48070b881
tdi-github <d1mmas28@gmail.com>
2024-10-19 09:29:22 +0300

    controller - if wrong event

--- api/app/Http/Controllers/JivoWebhookController.php
+++ api/app/Http/Controllers/JivoWebhookController.php
@@ -8,10 +8,30 @@ class JivoWebhookController extends Controller
 {
     public function webhook(Request $request)
     {
+        $request_only = [
+            'offline_message', 
+            'chat_finished'
+        ];
+
+        $event_name = $request->event_name;
+
+        // if wrong event_name
+        if(!in_array($event_name, $request_only)) 
+        {
+            return response()
+                ->json([
+                    'ok' => false
+                ]);
+        }
+
+
 
         return response()
             ->json([
-                'ok' => true
+                'ok' => true,
+                'request_test' => [
+                    'event_name' => $request->event_name
+                ]
             ]);
     } 
 }

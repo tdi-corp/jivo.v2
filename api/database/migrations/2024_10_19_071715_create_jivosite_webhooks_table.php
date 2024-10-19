5744fded1dbfb941840ae3cb362b9f5a88b4d989
tdi-github <d1mmas28@gmail.com>
2024-10-19 11:09:52 +0300

    update migr jivo_webhooks

--- api/database/migrations/2024_10_19_071715_create_jivosite_webhooks_table.php
+++ api/database/migrations/2024_10_19_071715_create_jivosite_webhooks_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('jivosite_webhooks', function (Blueprint $table) {
             $table->id();
+            $table->string('event_name'); //+
+            $table->string('widget_id'); //+
+            $table->string('visitor_name'); //+
+            $table->string('visitor_email'); //+
+            $table->string('visitor_phone'); //+
+            $table->string('visitor_description'); //+
+            $table->string('visitor_number'); //+
+            $table->integer('chat_id'); //+
+            $table->string('session_geoip_country')->nullable(); //+
+            $table->string('session_geoip_region')->nullable(); //+
+            $table->string('session_geoip_city')->nullable(); //+
+            $table->string('session_utm_json_source')->nullable(); //+
+            $table->string('session_utm_json_campaign')->nullable(); //+
+            $table->string('session_utm_json_content')->nullable(); //+
+            $table->string('session_utm_json_medium')->nullable(); //+
+            $table->string('session_utm_json_term')->nullable(); //+
+            $table->string('page_title'); //+
+            $table->string('page_url'); //+
             $table->timestamps();
         });
     }

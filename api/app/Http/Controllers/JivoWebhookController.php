3d8c5409e26963ea05588126c7fcd696ca7a1a34
tdi-github <d1mmas28@gmail.com>
2024-10-19 16:34:45 +0300

    add validation to controller

--- api/app/Http/Controllers/JivoWebhookController.php
+++ api/app/Http/Controllers/JivoWebhookController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\JivositeWebhook;
 use App\Models\JivositeWebhookMessage;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class JivoWebhookController extends Controller
 {
@@ -23,8 +24,49 @@ class JivoWebhookController extends Controller
         {
             return response()
                 ->json([
-                    'ok' => false
+                    'ok' => false,
                 ]);
+        }
+
+        // return response()
+        // ->json([
+        //     'ok' => false,
+        //     'type' => $request->session['utm_json']['contentd'] ?? null
+        // ]);
+
+        $validator = Validator::make($request->all(), [
+            'event_name' => 'required',
+            'widget_id' => 'required',
+
+            'visitor' => 'required',
+            'visitor.name' => 'nullable',
+            'visitor.email' => 'nullable',
+            'visitor.phone' => 'nullable',
+            'visitor.description' => 'nullable',
+            'visitor.number' => 'required',
+            'session' => 'required',
+            'session.geoip' => 'required',
+            'session.geoip.country' => 'required',
+            'session.geoip.region' => 'required',
+            'session.geoip.city' => 'required',
+            'session.utm_json' => 'required',
+            'session.utm_json.source' => 'nullable',
+            'session.utm_json.campaign' => 'nullable',
+            'session.utm_json.content' => 'nullable',
+            'session.utm_json.medium' => 'nullable',
+            'session.utm_json.term' => 'nullable',
+            'chat_id' => 'required|numeric',
+            'page' => 'required',
+            'page.url' => 'required',        
+            'page.title' => 'nullable', 
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'ok' => false,
+                'error' => true,
+                'messages' => $validator->errors()
+            ], 400);
         }
 
 
@@ -37,21 +79,21 @@ class JivoWebhookController extends Controller
             $query = JivositeWebhook::create([
                 'event_name' => $request->event_name,
                 'widget_id' => $request->widget_id,
-                'visitor_name' => $visitor['name'],
-                'visitor_email' => $visitor['email'],
-                'visitor_phone' => $visitor['phone'],
-                'visitor_description' => $visitor['description'],
+                'visitor_name' => $visitor['name'] ?? null,
+                'visitor_email' => $visitor['email'] ?? null,
+                'visitor_phone' => $visitor['phone'] ?? null,
+                'visitor_description' => $visitor['description'] ?? null,
                 'visitor_number' => $visitor['number'],
                 'chat_id' => $request->chat_id,
                 'session_geoip_country' => $session['geoip']['country'],
                 'session_geoip_region' => $session['geoip']['region'],
                 'session_geoip_city' => $session['geoip']['city'],
-                'session_utm_json_source' => $session['utm_json']['source'],
-                'session_utm_json_campaign' => $session['utm_json']['campaign'],
-                'session_utm_json_content' => array_key_exists('content', $session['utm_json']) ?? $session['utm_json']['content'],
-                'session_utm_json_medium' => array_key_exists('medium', $session['utm_json']) ?? $session['utm_json']['medium'],
-                'session_utm_json_term' => array_key_exists('term', $session['utm_json']) ?? $session['utm_json']['term'],
-                'page_title' => $page['title'],
+                'session_utm_json_source' => $session['utm_json']['source'] ?? null,
+                'session_utm_json_campaign' => $session['utm_json']['campaign'] ?? null,
+                'session_utm_json_content' => $session['utm_json']['content'] ?? null,
+                'session_utm_json_medium' => $session['utm_json']['medium'] ?? null,
+                'session_utm_json_term' => $session['utm_json']['term'] ?? null,
+                'page_title' => $page['title'] ?? null,
                 'page_url' => $page['url'],
             ]);
 
@@ -61,9 +103,7 @@ class JivoWebhookController extends Controller
             $messages = $event_name == $request_only[0] 
                 ? [[
                     'message' => $request->message,
-                    'timestamp' => now('UTC')->getTimestamp(),
-                    'type' => 'visitor',
-                    'agent_id' => null,                
+                    'type' => 'visitor',              
                 ]]
                 : $request->chat['messages'];
 
@@ -72,9 +112,9 @@ class JivoWebhookController extends Controller
                 JivositeWebhookMessage::create([
                     'jivosite_webhook_id' => $query->id,
                     'message' => $msg['message'],
-                    'timestamp' => $msg['timestamp'],
+                    'timestamp' => $msg['timestamp'] ?? now('UTC')->getTimestamp(),
                     'type' => $msg['type'],
-                    'agent_id' => array_key_exists('agent_id', $msg) ? $msg['agent_id'] : null,
+                    'agent_id' => $msg['agent_id'] ?? null,
         
                 ]);
             }
